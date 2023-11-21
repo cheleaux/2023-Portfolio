9230017d7bc4cb664496ac8acdad89761a1ee46f
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-11-21 16:21:00 +0000

    refactored about me func with js

--- AboutMe.js
+++ AboutMe.js
@@ -1,16 +1,23 @@
 const aboutMeText = document.querySelector('#abt-me-text')
+const firstParaAbtMe = document.querySelector('.abt-me-para-1')
+const wholeAbtMeText = document.querySelector('.abt-me-whole')
+
+checkAbtMeHeight()
 
 export default function resizeTextSection( e ) {
     const btn = e.target.closest('.abt-me-expand-btn')
     if( !btn ) return;
     aboutMeText.classList.toggle('abt-me-truncated')
-    changeBtnText(this)
+    checkAbtMeHeight()
+    setTimeout(changeBtnText(this), 0)
+}
+
+function checkAbtMeHeight(){
+    if ( aboutMeText.classList.contains('abt-me-truncated') ) aboutMeText.style.height = `${firstParaAbtMe.offsetHeight}px`;
+    else if ( !aboutMeText.classList.contains('abt-me-truncated') ) aboutMeText.style.height = `${wholeAbtMeText.offsetHeight}px`;    
 }
 
 function changeBtnText( btn ){
-    aboutMeText.offsetHeight > 110 ? btn.innerHTML = 'See Less' :
+    aboutMeText.offsetHeight == firstParaAbtMe.offsetHeight ? btn.innerHTML = 'See Less' :
     btn.innerHTML = 'See More';
 }
-
-
-
